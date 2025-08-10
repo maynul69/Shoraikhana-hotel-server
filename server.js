7ed49f64a227f33d2afb3be50ca1de42323a508c
MD. Maynul Hossain <maynulhossain69@gmail.com>
2025-08-10 16:14:57 +0600

    clerk setup done

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ import "dotenv/config";
 import cors from "cors"
 import { get } from "mongoose";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebHooks.js";
+
 
 connectDB()
 
 const app = express();
 app.use(cors()) //enable cross-origin resoure sharing
 
-app.get('/', (req, res) => res.send("API is Working Fine"))
+// middleware 
+app.use(express.json()) // parse json data`
+app.use(clerkMiddleware()) // clerk middleware for authentication
+
+// API to listen clerk webhooks
+
+app.use("/api/clerk", clerkWebhooks);
+
+app.get('/', (req, res) => res.send("API is Working "))
 
 const PORT = process.env.PORT || 3000;
 
